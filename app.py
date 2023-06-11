0c47b399fc55b3f194672c56ed8507c551b3deb4
Thibault <tbmc33@gmail.com>
2023-06-11 22:39:26 +0200

    login from file

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 import flask
 from calendar_converter import get_calendar_text
+from dotenv import dotenv_values
 
 app = flask.Flask(__name__)
+
+env = dotenv_values(".env")
 
 
 @app.route("/")
@@ -9,12 +12,18 @@ def main_request_handler() -> flask.Response:
     try:
         username = flask.request.args.get("username")
         password = flask.request.args.get("password")
+        if username is None:
+            username = env.get("username")
+        if password is None:
+            password = env.get("password")
         if username is None or not any(username) or password is None or not any(password):
             raise Exception("Missing username and password")
 
         calendar_text = get_calendar_text(username, password)
 
-        return flask.Response(calendar_text)
+        return flask.Response(calendar_text, headers={
+            "Content-Type": "text/calendar; charset=utf-8"
+        })
     except Exception as e:
         return flask.Response(str(e))
 
