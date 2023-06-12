49c727f0f29c500a9f2398075d6f16313ad68fea
Thibault <tbmc33@gmail.com>
2023-06-13 00:45:28 +0200

    Change in description

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 import base64
 import json
 
@@ -10,39 +11,46 @@ app = flask.Flask(__name__)
 env = dotenv_values(".env")
 
 
+def request_handler() -> flask.Response:
+    username: str | None = None
+    password: str | None = None
+
+    data_b64 = flask.request.args.get("data")
+    if data_b64 is not None:
+        decoded = base64.b64decode(data_b64.encode("utf-8"))
+        data = json.loads(decoded)
+        username = data["username"]
+        password = data["password"]
+
+    if username is None or password is None:
+        username = flask.request.args.get("username")
+        password = flask.request.args.get("password")
+
+    if username is None or password is None:
+        username = env.get("username")
+        password = env.get("password")
+    if username is None or not any(username) or password is None or not any(password):
+        raise Exception("Missing username and password")
+
+    calendar_converter = CalendarConverter()
+    calendar_text = calendar_converter.get_calendar_text(username, password)
+
+    return flask.Response(
+        calendar_text,
+        headers={
+            "Content-Type": "text/calendar; charset=utf-8"
+        },
+        mimetype="text/calendar",
+    )
+
+
 @app.route("/")
 def main_request_handler() -> flask.Response:
+    if os.environ.get("DEBUG"):
+        return request_handler()
+
     try:
-        username: str | None = None
-        password: str | None = None
-
-        data_b64 = flask.request.args.get("data")
-        if data_b64 is not None:
-            decoded = base64.b64decode(data_b64.encode("utf-8"))
-            data = json.loads(decoded)
-            username = data["username"]
-            password = data["password"]
-
-        if username is None or password is None:
-            username = flask.request.args.get("username")
-            password = flask.request.args.get("password")
-
-        if username is None or password is None:
-            username = env.get("username")
-            password = env.get("password")
-        if username is None or not any(username) or password is None or not any(password):
-            raise Exception("Missing username and password")
-
-        calendar_converter = CalendarConverter()
-        calendar_text = calendar_converter.get_calendar_text(username, password)
-
-        return flask.Response(
-            calendar_text,
-            headers={
-                "Content-Type": "text/calendar; charset=utf-8"
-            },
-            mimetype="text/calendar",
-        )
+        return request_handler()
     except Exception as e:
         return flask.Response(str(e))
 
