ae74c3d438d81e33d4492b95850793e4781399e5
Thibault <tbmc33@gmail.com>
2023-06-13 00:45:28 +0200

    Change in description

--- calendar_converter.py
+++ calendar_converter.py
@@ -15,13 +15,136 @@ PLAYED_WORDS = "played", "present", "available"
 EVENT_TYPE = dict[str, int | str | Any | list[Any] | dict[str, Any]]
 TIMEZONE = pytz.timezone("UTC")
 
+ORDER_PRESENT = {
+    "available": 50,
+    "played": 51,
+    "present": 52,
+    "rsvp": 40,
+    "not_selected": 30,
+    "unavailable": 10,
+    "not_played": 11,
+}
+
+MY_PRESENCE = {
+    "available": "CONFIRMED",
+    "played": "CONFIRMED",
+    "present": "CONFIRMED",
+    "rsvp": "NEEDS-ACTION",
+    "not_selected": "CANCELLED",
+    "unavailable": "CANCELLED",
+    "not_played": "CANCELLED",
+}
+
 
 def normalize(any_str: str) -> str:
     return unicodedata.normalize("NFD", any_str)
 
 
+def _extract_event_dates(event_data: EVENT_TYPE, event: Event) -> None:
+    start_date = datetime.fromisoformat(cast(str, event_data["start_at"]))
+    start_date = start_date.astimezone(TIMEZONE)
+    end_date_str = cast(str | None, event_data["end_at"])
+    if end_date_str is not None:
+        end_date = datetime.fromisoformat(end_date_str)
+    else:
+        end_date = start_date + timedelta(hours=2)
+    end_date = end_date.astimezone(TIMEZONE)
+
+    event.add("dtstart", start_date)
+    event.add("dtstamp", start_date)
+    event.add("dtend", end_date)
+
+
+def _extract_event_location(event_data: EVENT_TYPE, event: Event) -> None:
+    location = normalize(
+        cast(
+            dict[str, str | Any],
+            event_data["location"]
+        )["formatted_address"]
+    )
+    event.add("location", location)
+
+
+def _extract_event_summary(event_data: EVENT_TYPE, event: Event, team_name: str) -> None:
+    name = normalize(cast(str, event_data["name"]))
+    if team_name not in name:
+        summary = f"{team_name} - {name}"
+    else:
+        summary = name
+
+    # opponent_right = cast(dict[str, int | str | None] | None, event_data["opponent_right"])
+    # opponent_left = cast(dict[str, int | str | None] | None, event_data["opponent_left"])
+    # if opponent_left is not None and opponent_right is not None:
+    #     if opponent_left["id"] == team_id:
+    #         opponent_name = opponent_right["short_name"]
+    #     else:
+    #         opponent_name = opponent_left["short_name"]
+    #     summary += f" - contre {opponent_name}"
+
+    me_object = event_data.get("me", {})
+    if me_object is not None and me_object.get("group") is not None:
+        group = cast(dict[str, str], me_object.get("group", {}))
+        localized_name_presence = group.get("localized_name")
+        if localized_name_presence is not None:
+            summary += f" - {normalize(localized_name_presence)}"
+
+        slug_presence = group.get("slug_name")
+        presence = MY_PRESENCE.get(slug_presence)
+        event.add("status", presence)
+
+    event.add("summary", summary)
+
+
+def _extract_event_description(event_data: EVENT_TYPE, event: Event) -> None:
+    attendance_groups = cast(list[dict[str, int | str]] | None, event_data["attendance_groups"])
+    description = ""
+    if attendance_groups is not None:
+        attendance_group_list: list[tuple[int, str, int]] = []
+        for ppc in attendance_groups:
+            slug_sort_value = ORDER_PRESENT.get(cast(str, ppc["slug_name"]), 0)
+            attendance_group_list.append((
+                slug_sort_value,
+                cast(str, ppc["localized_name"]),
+                cast(int, ppc["count"])
+            ))
+        attendance_group_list.sort(reverse=True)
+
+        description = "\n".join([
+            f"{localized_name}: {count}"
+            for _, localized_name, count in attendance_group_list
+        ])
+
+    event.add("description", description.strip())
+
+
+def event_to_calendar_event(team_name: str, event_data: EVENT_TYPE) -> Event:
+    event = Event()
+    event.add("uid", str(event_data["id"]) + "@sporteasy.net")
+    _extract_event_location(event_data, event)
+    _extract_event_dates(event_data, event)
+    _extract_event_summary(event_data, event, team_name)
+    _extract_event_description(event_data, event)
+
+    event.add("class", "PUBLIC")
+    event.add("sequence", 0)
+    event.add("transp", "OPAQUE")
+
+    # todo: change this
+    event.add("created", datetime(2020, 1, 1, 1, 1, 1))
+    event.add("last-modified", datetime(2020, 1, 1, 1, 1, 1))
+
+    category_name = normalize(
+        cast(
+            dict[str, str | Any],
+            event_data["category"]
+        )["localized_name"]
+    )
+
+    return event
+
+
 class CalendarConverter:
-    def __init__(self):
+    def __init__(self) -> None:
         self.session_requests = requests.Session()
 
     def login(self, username: str, password: str) -> str:
@@ -51,83 +174,6 @@ class CalendarConverter:
         data: list[EVENT_TYPE] = response.json()["results"]
         return data
 
-    def event_to_calendar_event(self, team_id: int, team_name: str, event_data: EVENT_TYPE) -> Event:
-        id_ = event_data["id"]
-
-        start_date = datetime.fromisoformat(cast(str, event_data["start_at"]))
-        start_date = start_date.astimezone(TIMEZONE)
-        end_date_str = cast(str | None, event_data["end_at"])
-        if end_date_str is not None:
-            end_date = datetime.fromisoformat(end_date_str)
-        else:
-            end_date = start_date + timedelta(hours=2)
-        end_date = end_date.astimezone(TIMEZONE)
-
-        location = normalize(
-            cast(
-                dict[str, str | Any],
-                event_data["location"]
-            )["formatted_address"]
-        )
-        attendance_groups = cast(list[dict[str, int | str]] | None, event_data["attendance_groups"])
-
-        event = Event()
-        event.add("uid", str(id_) + "@sporteasy.net")
-
-        category_name = normalize(
-            cast(
-                dict[str, str | Any],
-                event_data["category"]
-            )["localized_name"]
-        )
-        name = normalize(cast(str, event_data["name"]))
-        if team_name not in name:
-            summary = f"{team_name} - {name}"
-        else:
-            summary = name
-
-        # opponent_right = cast(dict[str, int | str | None] | None, event_data["opponent_right"])
-        # opponent_left = cast(dict[str, int | str | None] | None, event_data["opponent_left"])
-        # if opponent_left is not None and opponent_right is not None:
-        #     if opponent_left["id"] == team_id:
-        #         opponent_name = opponent_right["short_name"]
-        #     else:
-        #         opponent_name = opponent_left["short_name"]
-        #     summary += f" - contre {opponent_name}"
-
-        event.add("summary", summary)
-        event.add("dtstart", start_date)
-        event.add("dtstamp", start_date)
-        event.add("dtend", end_date)
-        event.add("class", "PUBLIC")
-        event.add("description", summary)
-        event.add("sequence", 0)
-        event.add("transp", "OPAQUE")
-
-        # todo: change this
-        event.add("created", datetime(2020, 1, 1, 1, 1, 1))
-        event.add("last-modified", datetime(2020, 1, 1, 1, 1, 1))
-        event.add("status", "CONFIRMED")
-
-        description = ""
-        if attendance_groups is not None:
-            present_people_count = sum(
-                cast(int, ppc["count"])
-                for ppc in attendance_groups
-                if cast(str, ppc["slug_name"]).lower() in PLAYED_WORDS
-            )
-            not_present_people_count = sum(
-                cast(int, ppc["count"])
-                for ppc in attendance_groups
-                if cast(str, ppc["slug_name"]).lower() not in PLAYED_WORDS
-            )
-            description += f"\nPresents: {present_people_count} | Pas presents {not_present_people_count}"
-
-        event.add("description", description)
-        event.add("location", location)
-
-        return event
-
     def get_calendar_text(self, username: str, password: str) -> str:
         self.login(username, password)
         teams = self.list_teams()
@@ -147,7 +193,7 @@ class CalendarConverter:
             events = self.list_events(team_id)
             for event in events:
                 cal.add_component(
-                    self.event_to_calendar_event(team_id, team_name, event)
+                    event_to_calendar_event(team_name, event)
                 )
 
         text_calendar: str = cal \
