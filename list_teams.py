a1b04d2bedfbf9cf9446da2cf98e6d6e8e7fb8e9
Thibault Ballier <thibault.ballier@raisepartner.com>
2023-09-20 16:49:27 +0200

    feat: add team filter
    fix: #1

---
+++ list_teams.py
@@ -0,0 +1,17 @@
+from calendar_converter import CalendarConverter, load_env_data
+
+
+username, password, _ = load_env_data()
+calendar_converter = CalendarConverter()
+calendar_converter.login(username, password)
+
+teams = calendar_converter.list_teams()
+
+team_list = [
+    ("TEAM NAME", "TEAM ID"),
+] + teams
+
+str_template = "{:^8} | {}"
+print(str_template.format("TEAM ID", "TEAM NAME"))
+for team_id, team_name in teams:
+    print(str_template.format(team_id, team_name))
