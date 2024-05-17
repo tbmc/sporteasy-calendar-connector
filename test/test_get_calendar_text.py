b0b91839e35acaef74652d784cb4fd6f8b3aac97
Thibault <tbmc33@gmail.com>
2024-05-17 19:08:38 +0200

    test

--- test/test_get_calendar_text.py
+++ test/test_get_calendar_text.py
@@ -20,7 +20,7 @@ from .test_utils import (
     "calendar_connector.datetime_utils.get_current_datetime",
     return_value=datetime.datetime(2024, 12, 25, 10, 45, 0),
 )
-def test_get_calendar_text(
+def test_get_calendar_text_without_links(
     timestamp_mock: MagicMock,
     datetime_mock: MagicMock,
 ) -> None:
@@ -31,7 +31,7 @@ def test_get_calendar_text(
     mocked_response_events = read_text_by_name("list_events.json")
 
     expected_calendar = replace_unwanted_lines(
-        read_text_by_name("expected_calendar.ics")
+        read_text_by_name("expected_calendar_without_links.ics")
     )
 
     with requests_mock.Mocker() as request_mocker:
@@ -48,6 +48,47 @@ def test_get_calendar_text(
         converter = calendar_connector.calendar_converter.CalendarConverter()
         calendar_text = converter.get_calendar_text(
             "username", "password", False, "http://localhost:5000/", "1"
+        )
+
+    result = replace_unwanted_lines(calendar_text)
+    assert result == expected_calendar
+
+
+@patch(
+    "calendar_connector.datetime_utils.get_current_timestamp", return_value=173512350
+)
+@patch(
+    "calendar_connector.datetime_utils.get_current_datetime",
+    return_value=datetime.datetime(2024, 12, 25, 10, 45, 0),
+)
+def test_get_calendar_text_with_links(
+    timestamp_mock: MagicMock,
+    datetime_mock: MagicMock,
+) -> None:
+    importlib.reload(calendar_connector.calendar_converter)
+    importlib.reload(calendar_connector.event_convertor)
+
+    mocked_response_teams = read_text_by_name("list_teams.json")
+    mocked_response_events = read_text_by_name("list_events.json")
+
+    expected_calendar = replace_unwanted_lines(
+        read_text_by_name("expected_calendar_with_links.ics")
+    )
+
+    with requests_mock.Mocker() as request_mocker:
+        request_mocker.post(
+            url_authenticate,
+            status_code=200,
+            cookies={"sporteasy": "token test calendar"},
+        )
+        request_mocker.get(url_list_teams, text=mocked_response_teams)
+        request_mocker.get(
+            url_list_events.format(team_id=1), text=mocked_response_events
+        )
+
+        converter = calendar_connector.calendar_converter.CalendarConverter()
+        calendar_text = converter.get_calendar_text(
+            "username", "password", True, "http://localhost:5000/", "1"
         )
 
     result = replace_unwanted_lines(calendar_text)
